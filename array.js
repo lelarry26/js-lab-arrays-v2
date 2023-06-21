18ff211b3e8149b61fda6b066ff2a11535341191
Larry Le <larrygsu@gmail.com>
2023-06-20 23:24:24 -0400

    Updating with a few completed exercises

---
+++ array.js
@@ -0,0 +1,30 @@
+//1. Create a new array called `planets` that holds all of the names of the planets
+let planet = ["Earth", "Jupiter", "Neptune", "Mars", "Saturn", "Mercury", "Uranus", "Venus"]
+//console.log(planet);
+
+//2. What is the index of **Mars**? 
+//[3]
+//3. Print **Mars** to the command line from the `planet` array.
+//console.log(planet[3]);
+
+//4. Print **Saturn** to the command line from the `planet` array.
+//console.log(planet[4]);
+
+//5. Print **Venus** to the command line from the `planet` array.
+//console.log(planet[7]);
+
+//6. Print out how many elements are in the `planet` array.
+//console.log (planet.length)
+
+//7. Loop through the `planet` array using a for loop and print the name of each planet.
+//for (let thisWordCanBeAnythingRelevant = 0; thisWordCanBeAnythingRelevant < planet.length; thisWordCanBeAnythingRelevant++){
+   // console.log(planet[thisWordCanBeAnythingRelevant]);}
+
+//8. Create a while loop that iterates through each of the items in the `planet` array
+//and changes each element to lowercase.
+//planet.forEach(index => planet.push(index.toLowerCase()));
+//for (let index = 0; index < planet.length; index++){
+//console.log(planet[index])}
+while (planet = 0) {
+   console.log (planet);
+}
